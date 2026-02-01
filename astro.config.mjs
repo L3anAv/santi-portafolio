9d6fcae5ff3d5d7b92f8b52750e763ba4669e1f1
Leandro Avila - L3anAv <leandroav.dev@gmail.com>
2026-02-01 16:03:38 -0300

    fix: rutas de img

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://l3anav.github.io',
-    base: '/santi-portafolio',
+   site: 'https://l3anav.github.io',
+  base: '/santi-portafolio',
 });
